e7297f394798881e4a0b4a87820f53313c38ba9a
Student <liam@macbook-pro-byrne.local>
2016-11-02 14:25:26 -0400

    Added a button and colour to the button. We also added the dimensions for the button.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,6 +13,13 @@ class RatingControl: UIView {
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
+        
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+    button.backgroundColor = UIColor.red
+    addSubview(button)
+    func intrinsicContentSize() -> CGSize {
+            return CGSize(width: 240, height: 44)
+    }
     }
     
 }
