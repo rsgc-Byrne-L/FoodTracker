08781755d22bf05bd63603a0f5b792466577bcd9
Student <liam@macbook-pro-byrne.local>
2016-11-02 14:19:53 -0400

    Finished creating a custom class and setting up the dimensions

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Liam Byrne on 2016-11-02.
+//  Copyright © 2016 Liam Byrne. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    }
+    
+}
