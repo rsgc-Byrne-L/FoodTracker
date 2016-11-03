5fc3eedab90ca1e82d80a1f7a0f187f7ac71882d
Student <liam@macbook-pro-byrne.local>
2016-11-03 13:01:53 -0400

    Added a for loop around where the buttons are created to keep track of them in a an array

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,24 +9,31 @@
 import UIKit
 
 class RatingControl: UIView {
+    // MARK: Properties
+    
+    var rating = 0
+    var ratingButtons = [UIButton]()
     
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.red
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
-        addSubview(button)
-        func intrinsicContentSize() -> CGSize {
-            return CGSize(width: 240, height: 44)
+        for _ in 0..<5 {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            button.backgroundColor = UIColor.red
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
+            ratingButtons += [button]
+            addSubview(button)
+            func intrinsicContentSize() -> CGSize {
+                return CGSize(width: 240, height: 44)
+            }
         }
     }
     
     override public var intrinsicContentSize: CGSize {
         get {
             
-        return CGSize(width: 240, height: 44)
+            return CGSize(width: 240, height: 44)
         }
     }
     
