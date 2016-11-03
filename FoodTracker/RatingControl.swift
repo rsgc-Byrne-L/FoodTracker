03cc562a0ac56c01990a50e78898f5ee8b4593ca
Student <liam@macbook-pro-byrne.local>
2016-11-03 12:59:15 -0400

    Added function to the red button where it says "Button Pressed" in console when it is pressed

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,14 +12,27 @@ class RatingControl: UIView {
     
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
+        super.init(coder: aDecoder)
         
-    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-    button.backgroundColor = UIColor.red
-    addSubview(button)
-    func intrinsicContentSize() -> CGSize {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.red
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
+        addSubview(button)
+        func intrinsicContentSize() -> CGSize {
             return CGSize(width: 240, height: 44)
+        }
     }
+    
+    override public var intrinsicContentSize: CGSize {
+        get {
+            
+        return CGSize(width: 240, height: 44)
+        }
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
     }
     
 }
