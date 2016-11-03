aff50fd8eeb1d8fa78558d225da887fad98da305
Student <liam@macbook-pro-byrne.local>
2016-11-03 13:13:23 -0400

    Added lots of optimization for further upgrades with variables instead of numbers.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,14 +13,18 @@ class RatingControl: UIView {
     
     var rating = 0
     var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
     
     // MARK: Initialization
     override func layoutSubviews() {
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         
-        // Offset each button's origin by the length of the button plus spacing.
+        // Offset each button's origin by the length of the button plus some spacing.
         for (index, button) in ratingButtons.enumerated() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
             button.frame = buttonFrame
         }
     }
@@ -28,7 +32,7 @@ class RatingControl: UIView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
             let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
             button.backgroundColor = UIColor.red
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
